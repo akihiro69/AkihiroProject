008920c11038a1848530d1bd4dba24760efbe9ba
akihiro69 <78153278+akihiro69@users.noreply.github.com>
2022-03-04 20:30:06 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -187,8 +187,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ GeezProjects ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/GeezSupport)"
+                    text = "**✘ AkihiroProject ERROR REPORT ✘**\n\n"
+                    link = "[Group Support](https://t.me/AkihiroSupport)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
@@ -197,7 +197,7 @@ def register(**args):
                     ftext += "\nkami hanya mencatat fakta error dan tanggal,"
                     ftext += "\nkami menghormati privasi Anda."
                     ftext += "\nJika mau, Anda bisa melaporkan error ini,"
-                    ftext += "\ncukup forward saja pesan ini ke @GeezSupport"
+                    ftext += "\ncukup forward saja pesan ini ke @AkihiroSupport"
                     ftext += "\n================================\n\n"
                     ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
                     ftext += "\nTanggal : " + date

d35b05e5439aa893b7041112297125ac42213180
akihiro69 <78153278+akihiro69@users.noreply.github.com>
2022-03-12 10:27:03 +0700

    AkihiroProject Update 2.0.1 __init.py__

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -497,7 +497,7 @@ with bot:
                 result = builder.photo(
                     file=logogeez,
                     link_preview=False,
-                    text= f" 🈴 **akihirouserbot** 🈴 \n🗃️ ••**Jumlah Module:** `{len(dugmeler)}`\n📜 ••**Pemilik:** [{user.first_name}](tg://user?id={user.id})\n\n⚙️ **InLine Menu** ⚙️",
+                    text= f"**akihirouserbot**🈴\n🗃️**Jumlah Module:** `{len(dugmeler)}`\n**Pemilik:** [{user.first_name}](tg://user?id={user.id})\n\n**InLine Menu**",
                     buttons=buttons,
                 )
             elif query.startswith("repo"):
@@ -575,7 +575,7 @@ with bot:
             if event.query.user_id == uid or event.query.user_id in SUDO_USERS:
                 current_page_number = int(looters)
                 buttons = paginate_help(current_page_number, dugmeler, "helpme")
-                text = f" 🈴 **akihirouserbot** 🈴 \n🗃️ ••**Jumlah Module:** `{len(dugmeler)}`\n📜 ••**Pemilik:** [{user.first_name}](tg://user?id={user.id})\n\n⚙️ **InLine Menu** ⚙️"
+                text = f"**akihirouserbot**🈴\n**Jumlah Module:** `{len(dugmeler)}`\n**Pemilik:** [{user.first_name}](tg://user?id={user.id})\n\n**InLine Menu**"
                 await event.edit(
                     text,
                     file=logogeez,

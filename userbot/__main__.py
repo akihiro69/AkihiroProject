21aa7bed0be39afcaf0c04ccf1fb04b050861739
akihiro69 <78153278+akihiro69@users.noreply.github.com>
2022-03-28 12:03:27 +0700

    AkihiroProject Update 2.0.1 __main.py__

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,27 +1,23 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-# Copyright (C) 2021 TeamUltroid for autobot
 """ Userbot start point """
 
+
 import sys
+import requests
 from importlib import import_module
 
-import requests
 from pytgcalls import idle
-from telethon.tl.functions.channels import InviteToChannelRequest
 
-from userbot import BOT_TOKEN, BOT_USERNAME, BOT_VER, BOTLOG_CHATID
-from userbot import DEVS, LOGS, bot, branch, call_py
+from userbot import BOT_TOKEN, BOT_VER, blacklistgeez
+from userbot import DEVS, LOGS, LOOP, bot, call_py, BOTLOG_CHATID
+from userbot.clients import geez_userbot_on, multigeez
 from userbot.modules import ALL_MODULES
-from userbot.utils import autobot, checking
+from userbot.utils import autobot, autocreategroup, checking
 
 try:
+    client = multigeez()
+    total = 5 - client
     bot.start()
     call_py.start()
-    idle()
     user = bot.get_me()
     blacklistgeez = requests.get(
         "https://raw.githubusercontent.com/vckyou/Reforestation/master/blacklistgeez.json"
@@ -30,9 +26,9 @@ try:
         LOGS.warning(
             "NAMPAKNYA USERBOT TIDAK DAPAT BEKERJA, MUNGKIN ANDA TELAH DI BLACKLIST OLEH PEMILIK USERBOT.\nCredits: @VckyouuBitch"        )
         sys.exit(1)
-    if 5155140917 not in DEVS:
+    if 874946835 not in DEVS:
         LOGS.warning(
-            f"EOL\nAkihiroProject v{BOT_VER}, Copyright © 2021-2022 AKIHIRO69 <https://github.com/akihiro69>"
+            f"EOL\nGeezProjects v{BOT_VER}, Copyright © 2021-2022 VICKY <https://github.com/vckyou>"
         )
         sys.exit(1)
 except Exception as e:
@@ -42,34 +38,22 @@ except Exception as e:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info(
-    f"Join Grup https://t.me/AkihiroSupport"
-)
-
-LOGS.info(f"🍭 Akihiro - UserBot Berhasil Diaktifkan 🍭",
-            )
-
-async def geez_userbot_on():
-    try:
-        if BOTLOG_CHATID != 0:
-            await bot.send_message(
-                BOTLOG_CHATID,
-                f"🍭 **Akihiro - UserBot Sudah Nyalaaaa..!!** 🍭\n╼┅━━━━━╍━━━━━┅╾\n❍▹ `Bot Of :` [{user.first_name}](tg://user?id={user.id})\n❍▹ `BotVer :` {BOT_VER}@{branch}\n╼┅━━━━━╍━━━━━┅╾",
-            )
-    except Exception as e:
-        LOGS.info(str(e))
-    try:
-        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
-    except BaseException:
-        pass
+LOGS.info(f"Total Clients = {total} User")
+LOGS.info(f"Jika {user.first_name}
+LOGS.info(f"💢 AkihiroProject Berhasil Diaktfikan 💢")
 
 
-bot.loop.run_until_complete(checking())
-bot.loop.run_until_complete(geez_userbot_on())
+LOOP.run_until_complete(geez_userbot_on())
+LOOP.run_until_complete(checking())
+if not BOTLOG_CHATID:
+    LOOP.run_until_complete(autocreategroup())
 if not BOT_TOKEN:
-    bot.loop.run_until_complete(autobot())
+    LOOP.run_until_complete(autobot())
 idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
-    bot.run_until_disconnected()
+    try:
+        bot.run_until_disconnected()
+    except ConnectionError:
+        pass

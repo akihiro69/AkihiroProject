d5e61122ea9be36c17d1d68cab2f20875cc9059b
akihiro69 <78153278+akihiro69@users.noreply.github.com>
2022-03-29 17:37:18 +0700

    AkihiroProject Update 2.0.1 vcplugin.py

--- userbot/modules/vcplugin.py
+++ userbot/modules/vcplugin.py
@@ -504,7 +504,7 @@ async def join_(event):
         stream_type=StreamType().pulse_stream,
     )
     try:
-        await geezav.edit(f"`• joinvcs {chat_id}`")
+        await geezav.edit(f"• **Successfully Join VC :** `{chat_id}` •")
     except Exception as ex:
         await edit_delete(event, f"**ERROR:** `{ex}`")
 
@@ -519,7 +519,7 @@ async def leavevc(event):
             await call_py.leave_group_call(chat_id)
         except (NotInGroupCallError, NoActiveGroupCall):
             pass
-        await geezav.edit(f"`• leavevcs {chat_id}`")
+        await geezav.edit(f"• **Successfully Left VC :** `{chat_id}` •")
     else:
         await geezav.delete(f"`Sorry, {from_user} not in VC Group`")
 

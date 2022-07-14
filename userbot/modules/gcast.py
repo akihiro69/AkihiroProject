e09588bf4555f5692f6fb0892cb37950176cb1b6
akihirooo <78153278+akihiro69@users.noreply.github.com>
2022-07-14 08:33:43 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -39,7 +39,7 @@ async def gcast(event):
     elif event.is_reply:
         msg = await event.get_reply_message()
     else:
-        return await edit_delete(event, "**Berikan Sebuah Pesan atau Reply**")
+        return await edit_delete(event, "**Ketik ato Reply pesannya lah pantq.**")
     kk = await edit_or_reply(event, "`Globally Broadcasting Msg...`")
     er = 0
     done = 0
